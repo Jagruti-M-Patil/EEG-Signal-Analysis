f415989d161a455d825fc336d8aea874bcc11a0f
Souritra Garai <46619335+Souritra-Garai@users.noreply.github.com>
2023-05-10 02:29:10 +0530

    Completed categorizing into preictal etc

--- dataset.py
+++ dataset.py
@@ -1,35 +1,37 @@
-path = "E:\\Semester 2\\IN 791\\physionet.org\\files\\chbmit\\1.0.0"
+path = "D:\\Jagruti's Docs\\IN719\\CHB-MIT-EEG-Data"
+
+preictal_period = 15 * 60 # seconds
 
 cases = [
-    "chb01",
-	"chb02",
-	"chb03",
-	"chb04",
-	"chb05"
+    "chb12"
+	# "chb02",
+	# "chb03",
+	# "chb04",
+	# "chb05"
 ]
 
 EEG_Channels = (
-    "FP1-F7",
-	"F7-T7",
-	"T7-P7",
-	"P7-O1",
-	"FP1-F3",
-	"F3-C3",
-	"C3-P3",
-	"P3-O1",
-	"FP2-F4",
-	"F4-C4",
-	"C4-P4",
-	"P4-O2",
-	"FP2-F8",
-	"F8-T8",
-	"T8-P8",
-	"P8-O2",
-	"FZ-CZ",
-	"CZ-PZ",
-	"P7-T7",
-	"T7-FT9",
-	"FT9-FT10",
-	"FT10-T8",
-	"T8-P8"
+    "FP1-F7",	# 1
+	"F7-T7",	# 2
+	"T7-P7",	# 3
+	"P7-O1",	# 4
+	"FP1-F3",	# 5
+	"F3-C3",	# 6
+	"C3-P3",	# 7
+	"P3-O1",	# 8
+	"FP2-F4",	# 9
+	"F4-C4",	# 10
+	"C4-P4",	# 11
+	"P4-O2",	# 12
+	"FP2-F8",	# 13
+	"F8-T8",	# 14
+	"T8-P8",	# 15
+	"P8-O2",	# 16
+	"FZ-CZ",	# 17
+	"CZ-PZ",	# 18
+	"P7-T7",	# 19
+	"T7-FT9",	# 20
+	"FT9-FT10",	# 21
+	"FT10-T8",	# 22
+	"T8-P8",	# 23
 )
